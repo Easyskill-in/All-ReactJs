a3cc1ce8f661692059a6c3644308d0590b5bfa01
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-06-25 11:52:07 +0530

    first

---
+++ 10-12 batch/25-06-2026/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log("Hello")
+  return (<>
+    <h1>HTML TAG</h1> <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae tenetur odit voluptatum molestias asperiores ex, a, fuga architecto rerum velit modi nemo! Culpa et natus labore enim dolor maxime sequi?</p>
+  </>
+  )
+}
+
+
+export default App;
